96753612c109dfafe068647cd5a89ffaf2317a4c
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-01-16 20:36:03 -0700

    Add hash debug output to build

--- make/hash.py
+++ make/hash.py
@@ -11,12 +11,17 @@ from . import image_labels
 from . import image_exists
 from . import REPO
 
+from .config import parse_config
+
 kwds: dict[str, str] = {
     "help": "Get the variant hash",
 }
 
 
 def register(parser: ArgumentParser):
+    _ = parser.add_argument(
+        "--debug", action="store_true", help="Show all pieces of the hash"
+    )
     _ = parser.add_argument(
         "target",
         action="extend",
@@ -29,7 +34,66 @@ def register(parser: ArgumentParser):
 
 def command(args: Namespace):
     for target in cast(list[str], args.target):
-        print(f"{target}: {hash(target)[:9]}")
+        if not cast(bool, args.debug):
+            print(f"{target}: {hash(target)[:9]}")
+            continue
+
+        print(f"{target}: {hash(target)[:9]}\n  ", end="")
+        print("\n  ".join([f"{t.ljust(5)} {n}: {h}" for t, n, h in hash_parts(target)]))
+
+
+def _hash(value: bytes) -> str:
+    m = sha256()
+    m.update(value)
+    return m.hexdigest()[:9]
+
+
+def hash_parts(target: str) -> list[tuple[str, str, str]]:
+    parts: list[tuple[str, str, str]] = []
+    containerfile = (
+        f"variants/{target}.Containerfile"
+        if target != "rootfs"
+        else "rootfs.Containerfile"
+    )
+    # TODO also include hash from x-depends
+    if "-" in target and not os.path.exists(containerfile):
+        base_variant, template = target.rsplit("-", 1)
+        containerfile = f"templates/{template}.Containerfile"
+        image = f"{REPO}:{base_variant}"
+        labels = image_labels(image, not image_exists(image, False, False))
+        parts.append(("image", image, labels["hash"][:9]))
+
+    elif target != "rootfs":
+        _, config = parse_config(containerfile)
+        depends = config.get("depends", "rootfs")
+        image = f"{REPO}:{depends}"
+        labels = image_labels(image, not image_exists(image, False, False))
+        parts.append(("image", image, labels["hash"][:9]))
+
+    with open(containerfile, "rb") as f:
+        parts.append(("file", containerfile, _hash(f.read())))
+
+    for file in sorted(iglob(f"overlay/{target}/**", recursive=True)):
+        if os.path.isdir(file):
+            parts.append(("dir", file, _hash(file.encode("utf-8"))))
+
+        else:
+            with open(file, "rb") as f:
+                parts.append(("file", file, _hash(f.read())))
+
+    for file in sorted(
+        [
+            "build.py",
+            "hash.py",
+            "pull.py",
+            "push.py",
+            "__init__.py",
+        ]
+    ):
+        with open(f"make/{file}", "rb") as f:
+            parts.append(("file", f"make/{file}", _hash(f.read())))
+
+    return parts
 
 
 def hash(target: str) -> str:
