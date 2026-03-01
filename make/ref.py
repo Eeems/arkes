e1100c6aa30570a65041611c26a761eed202532f
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-03-01 16:52:26 -0700

    Ref name

---
+++ make/ref.py
@@ -0,0 +1,35 @@
+from argparse import ArgumentParser
+from argparse import Namespace
+from re import sub as re_sub
+from typing import Any
+from typing import cast
+
+kwds: dict[str, str] = {
+    "help": "What does this do?",
+}
+
+
+def register(parser: ArgumentParser):
+    _ = parser.add_argument("ref", help="ref name to convert")
+
+
+def ref(ref: str) -> str:
+    return re_sub(r"^[.-]+", "", re_sub(r"[^a-zA-Z0-9_.-]", "-", ref))
+
+
+def command(args: Namespace):
+    print(ref(cast(str, args.ref)))
+
+
+if __name__ == "__main__":
+    kwds["description"] = kwds["help"]
+    del kwds["help"]
+    parser = ArgumentParser(
+        **cast(  # pyright: ignore[reportAny]
+            dict[str, Any],  # pyright: ignore[reportExplicitAny]
+            kwds,
+        ),
+    )
+    register(parser)
+    args = parser.parse_args()
+    command(args)
