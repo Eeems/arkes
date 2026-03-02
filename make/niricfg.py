4a012ead7484e27fcf1752ceb985a6cf124e6b57
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-03-02 13:30:34 -0700

    Get build working

--- make/niricfg.py
+++ make/niricfg.py
@@ -12,7 +12,25 @@ kwds: dict[str, str] = {
 
 
 def register(parser: ArgumentParser) -> None:
-    _ = parser.add_argument("--build-image", action="store_true", dest="build_image")
+    _ = parser.add_argument(
+        "--build-image",
+        action="store_true",
+        dest="build_image",
+        help="Build the builder image before running the build",
+    )
+    _ = parser.add_argument(
+        "cmd",
+        nargs="*",
+        help="Command to run instead of normal build",
+        default=[
+            "uvx",
+            "flet",
+            "build",
+            "linux",
+            "--yes",
+            "--skip-flutter-doctor",
+        ],
+    )
 
 
 def command(args: Namespace) -> None:
@@ -31,9 +49,7 @@ def command(args: Namespace) -> None:
         "--volume=./tools/niricfg:/src:rw",
         "--workdir=/src",
         image,
-        "bash",
-        "-c",
-        "yes | flet build linux",
+        *cast(list[str], args.cmd),
     )
 
 
