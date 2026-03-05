b65a55b570f9ab20dca36b9fe21972d37887b83d
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-03-05 15:56:42 -0700

    Only build niricfg builder image, move app to new repo

--- make/niricfg.py
+++ make/niricfg.py
@@ -3,62 +3,25 @@ from argparse import Namespace
 from typing import Any
 from typing import cast
 
-from . import execute
 from . import podman
 from . import BUILDER
 
 kwds: dict[str, str] = {
-    "help": "Build the niricfg tool",
+    "help": "Build the niricfg tool builder",
 }
 
 
-def register(parser: ArgumentParser) -> None:
-    _ = parser.add_argument(
-        "--build-image",
-        action="store_true",
-        dest="build_image",
-        help="Build the builder image before running the build",
-    )
-    _ = parser.add_argument(
-        "--run",
-        action="store_true",
-        help="Run the application after building it",
-    )
-    _ = parser.add_argument(
-        "cmd",
-        nargs="*",
-        help="Command to run instead of normal build",
-        default=[
-            "uvx",
-            "flet",
-            "build",
-            "linux",
-            "--yes",
-            "--skip-flutter-doctor",
-        ],
-    )
+def register(_: ArgumentParser) -> None:
+    pass
 
 
-def command(args: Namespace) -> None:
-    image = f"{BUILDER}:niricfg"
-    if cast(bool, args.build_image):
-        podman(
-            "build",
-            f"--tag={image}",
-            "--file=tools/niricfg/Containerfile",
-            "tools/niricfg",
-        )
-
+def command(_: Namespace) -> None:
     podman(
-        "run",
-        "--rm",
-        "--volume=./tools/niricfg:/src:rw",
-        "--workdir=/src",
-        image,
-        *cast(list[str], args.cmd),
+        "build",
+        f"--tag={BUILDER}:niricfg",
+        "--file=tools/niricfg/Containerfile",
+        "tools/niricfg",
     )
-    if cast(bool, args.run):
-        execute("tools/niricfg/build/linux/niricfg")
 
 
 if __name__ == "__main__":
