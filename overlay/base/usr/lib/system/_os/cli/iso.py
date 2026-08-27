f7cf455332f8404e07b7742ce2dfff423af703e6
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-08-27 03:20:03 -0600

    Revert skopeo just for iso

--- overlay/base/usr/lib/system/_os/cli/iso.py
+++ overlay/base/usr/lib/system/_os/cli/iso.py
@@ -1,5 +1,6 @@
 import atexit
 import os
+import shlex
 import shutil
 import sys
 from argparse import (
@@ -20,9 +21,7 @@ from .. import (
 from ..podman import (
     export,
     podman,
-    storage_graph_driver,
-    storage_graph_root,
-    storage_run_root,
+    podman_cmd,
 )
 from ..system import (
     baseImage,
@@ -90,24 +89,26 @@ def iso() -> str:
 
     atexit.unregister(exitFunc1)
     podman("rmi", f"system:iso-{uuid}")
-    dest_storage = os.path.join(ROOTFS_PATH, "var/lib/containers/storage")
-    if os.path.isdir("/host-storage"):
-        src_root = "/host-storage"
-        src_runroot = "/host-runroot"
-        driver = os.environ.get("HOST_STORAGE_DRIVER", "overlay")
-
-    else:
-        src_root = storage_graph_root()
-        src_runroot = storage_run_root()
-        driver = storage_graph_driver()
-
     execute(
-        "skopeo",
-        "--insecure-policy",
-        "copy",
-        f"containers-storage:[{driver}@{src_root}+{src_runroot}]{buildImage}",
-        f"containers-storage:[overlay@{dest_storage}]{buildImage}",
+        "bash",
+        "-c",
+        " | ".join(
+            [
+                shlex.join(podman_cmd("save", buildImage)),
+                shlex.join(
+                    [
+                        "podman",
+                        f"--root={ROOTFS_PATH}/var/lib/containers/storage",
+                        "--runroot=/tmp/podman-runroot",
+                        "--storage-driver=overlay",
+                        "--events-backend=file",
+                        "load",
+                    ]
+                ),
+            ]
+        ),
     )
+    os.unlink(os.path.join(ROOTFS_PATH, "var/lib/containers/storage/db.sql"))
     _ = shutil.copytree(os.path.join(ROOTFS_PATH, "etc/system/archiso"), "archiso")
     for path in [
         "loader/entries/01-archiso-x86_64-linux.conf",
